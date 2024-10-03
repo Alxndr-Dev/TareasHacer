33c22467f07db36437a52eda56e3fd239c239f55
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-03 00:19:52 -0400

    Agregando Inquirer, version 7.3.3

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('colors');
 
-const {mostrarMenu, pausa} = require('./helpers/mensajes');
-
+const {inquirerMenu} = require('./helpers/inquirer');
 console.clear();
 
 const main = async() =>{
@@ -12,17 +11,11 @@ const main = async() =>{
     let opt = '';
 
     do{
-       opt = await mostrarMenu();
-       console.log({opt});
-
-       if(opt !== '0'){
-            await pausa();
-       }
-       
+        opt = await inquirerMenu();
+        console.log({opt});
+    
     } while( opt !== '0');
 
-
-    // pausa();
 
 }
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+require('colors');
+
+
+const preguntas =[
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Que desea hacer?',
+        choices: ['1. Crear Lista', 'opt2', 'opt3']
+    }
+]
+
+
+const inquirerMenu = async() =>{
+
+    //console.clear();
+    console.log('============================='.green);
+    console.log('   Seleccione una opcion'.green);
+    console.log('============================= \n'.green);
+
+
+    const opt = await inquirer.prompt(preguntas);
+    return opt;
+}
+
+module.exports ={
+    inquirerMenu
+}
