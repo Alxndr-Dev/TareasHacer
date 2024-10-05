fa0698167cc048a354379eecb524b0e9b86dd871
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-05 02:25:16 -0400

    Agregando la pausa al momento de seleccionar una opcion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('colors');
 
-const {inquirerMenu} = require('./helpers/inquirer');
+const {inquirerMenu, pausa} = require('./helpers/inquirer');
 console.clear();
 
 const main = async() =>{
@@ -13,6 +13,8 @@ const main = async() =>{
     do{
         opt = await inquirerMenu();
         console.log({opt});
+
+        if(opt !== '0') await pausa();
     
     } while( opt !== '0');
 
