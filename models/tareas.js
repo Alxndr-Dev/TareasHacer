4e399a39e7df9d0ee62cf43cbc176ab746f31f10
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-10-18 23:43:44 -0400

    Listando las tareas completadas e incompletas

--- models/tareas.js
+++ models/tareas.js
@@ -66,6 +66,34 @@ class Tareas {
 
     }
 
+    listarPendientesCompletadas( completadas = true ){
+
+        console.log();
+        let contador = 0;
+
+        this.listadoArr.forEach(( tarea )=>{
+
+        const {desc, completadoEn} = tarea;
+        const estado = ( completadoEn ) ? 'Completado'.green : 'Pendiente'.red;
+
+        if(completadas){
+            if(completadoEn){
+                contador += 1;
+                console.log(`${( contador + '.' ).green}  ${desc} :: ${completadoEn.green}`);
+            }
+        }else {
+            if(!completadoEn){
+                contador += 1;
+                console.log(`${( contador + '.' ).green}  ${desc} :: ${estado}`);
+            }
+        }
+        
+        
+        })
+
+    }
+
+
 }
 
 
